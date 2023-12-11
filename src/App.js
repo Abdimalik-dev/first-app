362ac8c29e85b3ddccab7efa7d63ffaf6562bb34
Abdimalik-dev <cabdixuseen076@gmail.com>
2023-12-11 21:14:52 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+  const User = {
+      Name:"Abdimalik",
+      Age: 21,
+      City: "Hargeisa",
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> Names: {User.Name} </p>
+      <p>Age: {User.Age}</p>
+      <p> City: {User.City} </p>
     </div>
   );
 }
