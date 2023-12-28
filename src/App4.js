f20fbab7c77707d3074a43287b15d21a98c09645
Abdimalik-dev <cabdixuseen076@gmail.com>
2023-12-28 22:51:10 +0300

    commit

--- src/App4.js
+++ src/App4.js
@@ -1,19 +1,19 @@
-function App() {
+
     const grocery = [
         {item:"milk", price:2},
         {item:"fanta", price:1.3},
         {item:"apple", price:4}
     ]
 
-
+export default function App() {
     return (
         <div className="App">
             <h1> List</h1>
-            {grocery.map((product)=>  <h1>{(product.item)  (product.price)}</h1>
+            {grocery.map((grocery )=> {
+             return <h1>{grocery}</h1>
   
-            )}
+})}
         </div>
-    );
+    ); 
 }
 
-export default App;  
